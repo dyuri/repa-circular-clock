848fcf6e59d87c1169721b3086ef1265b46700f6
Gyuri Horak <dyuri@horak.hu>
2022-03-08 17:28:32 +0100

    my-element => repa-circular-clock

--- test/repa-circular-clock_test.js
+++ test/repa-circular-clock_test.js
@@ -4,18 +4,18 @@
  * SPDX-License-Identifier: BSD-3-Clause
  */
 
-import {MyElement} from '../my-element.js';
+import {RepaCircularClock} from '../repa-circular-clock.js';
 import {fixture, assert} from '@open-wc/testing';
 import {html} from 'lit/static-html.js';
 
-suite('my-element', () => {
+suite('repa-circular-clock', () => {
   test('is defined', () => {
-    const el = document.createElement('my-element');
-    assert.instanceOf(el, MyElement);
+    const el = document.createElement('repa-circular-clock');
+    assert.instanceOf(el, RepaCircularClock);
   });
 
   test('renders with default values', async () => {
-    const el = await fixture(html`<my-element></my-element>`);
+    const el = await fixture(html`<repa-circular-clock></repa-circular-clock>`);
     assert.shadowDom.equal(
       el,
       `
@@ -27,7 +27,7 @@ suite('my-element', () => {
   });
 
   test('renders with a set name', async () => {
-    const el = await fixture(html`<my-element name="Test"></my-element>`);
+    const el = await fixture(html`<repa-circular-clock name="Test"></repa-circular-clock>`);
     assert.shadowDom.equal(
       el,
       `
@@ -39,7 +39,7 @@ suite('my-element', () => {
   });
 
   test('handles a click', async () => {
-    const el = await fixture(html`<my-element></my-element>`);
+    const el = await fixture(html`<repa-circular-clock></repa-circular-clock>`);
     const button = el.shadowRoot.querySelector('button');
     button.click();
     await el.updateComplete;
@@ -54,7 +54,7 @@ suite('my-element', () => {
   });
 
   test('styling applied', async () => {
-    const el = await fixture(html`<my-element></my-element>`);
+    const el = await fixture(html`<repa-circular-clock></repa-circular-clock>`);
     await el.updateComplete;
     assert.equal(getComputedStyle(el).paddingTop, '16px');
   });
