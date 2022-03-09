94b45bab50ea24a4eb1c5f0b1121dcb14e3d456c
Gyuri Horak <dyuri@horak.hu>
2022-03-09 10:26:57 +0100

    circular clock base

--- repa-circular-clock.js
+++ repa-circular-clock.js
@@ -1,13 +1,13 @@
 /**
  * @license
- * Copyright 2019 Google LLC
+ * Copyright 2022 Gyuri Horák <dyuri@horak.hu>
  * SPDX-License-Identifier: BSD-3-Clause
  */
 
 import {LitElement, html, css} from 'lit';
 
 /**
- * An example element.
+ * Circular clock element.
  *
  * @fires count-changed - Indicates when the count changes
  * @slot - This element has a slot
@@ -17,58 +17,121 @@ export class RepaCircularClock extends LitElement {
   static get styles() {
     return css`
       :host {
+        width: 800px;
+        height: 800px;
+        font-size: 2rem;
+        position: relative;
         display: block;
-        border: solid 1px gray;
-        padding: 16px;
-        max-width: 800px;
+        overflow: hidden;
+        margin: 20px auto;
+        background: transparent;
+        font-family: monospace;
+        color: #444;
+      }
+
+      .dial {
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        width: var(--dial-size, 100%);
+        height: var(--dial-size, 100%);
+        transform: translate(-50%, -50%);
+        border-radius: 50%;
+      }
+
+      .dial:before {
+        content: "";
+        inset: 0;
+        margin: auto;
+        position: absolute;
+        display: block;
+        width: calc(100% - .45em);
+        height: calc(100% - .45em);
+        border: 1.5em solid #444;
+        border-radius: 50%;
+        box-sizing: border-box;
+      }
+
+      .ring {
+        position: relative;
+        width: 100%;
+        height: 100%;
+        transition: all .5s;
+        border: 1.9em solid #333;
+        border-radius: 50%;
+        border-bottom-color: transparent;
+        box-sizing: border-box;
+        padding: 0;
+        margin: 0;
+        list-style: none;
+      }
+
+      .ring li {
+        position: absolute;
+        top: -1.5em;
+        left: -1.5em;
+        height: calc(100% + 3em);
+        width: calc(100% + 3em);
+        text-align: center;
+      }
+
+      .dial-day {
+        --dial-size: 90%;
+      }
+
+      .dial-month {
+        --dial-size: calc(90% - 4em);
+      }
+
+      .dial-dow {
+        --dial-size: calc(90% - 8em);
       }
     `;
   }
 
   static get properties() {
     return {
-      /**
-       * The name to say "Hello" to.
-       * @type {string}
-       */
-      name: {type: String},
-
-      /**
-       * The number of times the button has been clicked.
-       * @type {number}
-       */
-      count: {type: Number},
     };
   }
 
   constructor() {
     super();
-    this.name = 'World';
-    this.count = 0;
+  }
+
+  updated(changedProperties) {
+    super.updated(changedProperties);
+    this._rotateFields();
+  }
+
+  _rotateFields() {
+    const rings = this.shadowRoot.querySelectorAll('.ring');
+    rings.forEach(ring => {
+      const fields = ring.querySelectorAll('li');
+      const length = fields.length;
+      if (length > 0) {
+        const slice = 270 / length;
+        fields.forEach((field, index) => {
+          field.style.transform = `rotate(${-135 + slice / 2 + index * slice}deg)`;
+        });
+      }
+    });
   }
 
   render() {
     return html`
-      <h1>${this.sayHello(this.name)}!</h1>
-      <button @click=${this._onClick} part="button">
-        Click Count: ${this.count}
-      </button>
-      <slot></slot>
+      <div class="dials">
+        <div class="dial dial-day">
+          <ul class="ring">
+            <li>01</li>
+            <li>02</li>
+          </ul>
+        </div>
+        <div class="dial dial-month">
+        </div>
+        <div class="dial dial-dow">
+        </div>
+      </div>
     `;
-  }
-
-  _onClick() {
-    this.count++;
-    this.dispatchEvent(new CustomEvent('count-changed'));
-  }
-
-  /**
-   * Formats a greeting
-   * @param name {string} The name to say "Hello" to
-   * @returns {string} A greeting directed at `name`
-   */
-  sayHello(name) {
-    return `Hello, ${name}`;
   }
 }
 
